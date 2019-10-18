c61c55dd2115c6d06c9ecaf6e0324aa730a6a4e3
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-10-18 00:36:36 -0400

    Add project files.

---
+++ Controllers/DynamoDbController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamoDb.Libs.DynamoDb;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DynamoDb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/DynamoDb")]
+    public class DynamoDbController : Controller
+    {
+
+        private readonly IDynamoDbExamples _dynamoDbExample;
+
+        public DynamoDbController(IDynamoDbExamples dynamoDbExamples)
+        {
+            _dynamoDbExample = dynamoDbExamples;
+        }
+       
+        [Route("createtable")]
+        public IActionResult CreateDynamoDbTable()
+        {
+            _dynamoDbExample.CreateDynamoDbTable();
+
+            return Ok();
+        }
+    }
+}
